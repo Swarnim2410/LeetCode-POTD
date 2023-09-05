77b0762c70d7592869221d74e6d7a5ad2b15af29
Swarnim2410 <swarnimagupta2124@gmail.com>
2023-09-05 10:26:18 +0530

    5 September POTD

--- 5Sept.cpp
+++ 5Sept.cpp
@@ -1,19 +1,63 @@
+/*
+// Definition for a Node.
+class Node {
 public:
-    // Function to return the adjacency list for each vertex.
-    vector<vector<int>> printGraph(int v, vector<pair<int,int>>edges) 
-    {
-        vector<int>adj[v];
-        for(auto it : edges)
-        {
-            adj[it.first].push_back(it.second);
-            adj[it.second].push_back(it.first);
-        }
-        vector<vector<int>>ans;
-        for(int i=0;i<v;i++)
-        {
-            vector<int>temp = adj[i];
-            ans.push_back(temp);
-        }
-        return ans;
-        // Code here
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
     }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) 
+    {
+        struct Node* temp = head;
+        struct Node* head2 = NULL;
+        while(temp)
+        {
+            struct Node *newnode = new Node(temp->val);
+            if(head2==NULL)
+            {
+                head2=newnode;
+            }
+            else
+            {
+                struct Node *temp3 = head2;
+                while(temp3->next)
+                {
+                    temp3 = temp3 ->next;
+                }
+                temp3 -> next = newnode;
+            }
+            temp = temp ->next;
+        }
+        map<Node*, Node*>m;
+        temp = head;
+        struct Node *temp2 = head2;
+        while(temp)
+        {
+            m[temp] = temp2;
+            temp=temp->next;
+            temp2=temp2->next;
+        }
+        temp=head;
+        temp2=head2;
+        while(temp)
+        {
+            temp2->random = m[temp->random];
+            temp=temp->next;
+            temp2=temp2->next;
+        }
+        return head2;
+
+
+        
+    }
+};
