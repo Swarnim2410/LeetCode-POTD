f28f4a4c9d04eeb49c7395ca2be50cdc3b18476e
Swarnim2410 <swarnimagupta2124@gmail.com>
2023-12-17 23:24:14 +0530

    17 December POTD

---
+++ 17Dec.cpp
@@ -0,0 +1,37 @@
+class FoodRatings
+{
+public:
+    unordered_map<string, set<pair<int, string>>> mp;
+    unordered_map<string, string> ms;
+    unordered_map<string, int> mr;
+
+    FoodRatings(vector<string> &foods, vector<string> &cuisines, vector<int> &ratings)
+    {
+        int n = foods.size();
+        for (int i = 0; i < n; i++)
+        {
+            mr[foods[i]] = ratings[i];
+            mp[cuisines[i]].insert({-ratings[i], foods[i]});
+            ms[foods[i]] = cuisines[i];
+        }
+    }
+    void changeRating(string food, int newRating)
+    {
+        int prev = mr[food];
+        mr[food] = newRating;
+        mp[ms[food]].erase({-prev, food});
+        mp[ms[food]].insert({-newRating, food});
+    }
+
+    string highestRated(string cuisine)
+    {
+        return mp[cuisine].begin()->second;
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
